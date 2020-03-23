e9081c0cad3024d2a823e387c2e67c5ad0b0d732
Happy8-8 <gafonji@gmail.com>
2020-03-23 20:25:19 +0000

    rspec test completed

--- spec/controllers/cars_controller_spec.rb
+++ spec/controllers/cars_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe CarsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+  context "index action" do
+    it "should return a successful response" do
+      get :index
+      expect(response.status).to eq 200
+    end
+  end
+
+  context "new action" do
+    it "should redirect unauthenticated users" do
+      get :new
+      expect(response.status).to eq 302
+    end
+  end
 
 end
